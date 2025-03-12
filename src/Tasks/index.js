bdcb06ad2c333f374733adb298db8fbc5db97611
Lukasz Duszak <lukas94.webdeveloper@gmail.com>
2025-03-12 22:04:47 +0100

    commit join 2 section

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,22 +1,28 @@
 import "./style.css"
 
-const Tasks = ({tasks, hideDoneTasks}) => (
+const Tasks = ({ tasks, hideDoneTasks }) => (
    <ul className="tasksList">
       {tasks.map(task => (
-         <li  key={task.id}
-            className={`tasksList__element${task.done && hideDoneTasks ?
-          " tasksList__element--hidden" :
-          ""}`}
+         <li
+            key={task.id}
+            className={`tasksList__element${task.done && hideDoneTasks
+               ? " tasksList__element--hidden"
+               : ""}`
+            }
          >
-           <button 
-            className={`buttonTask ${task.done ? "button__tasksListItem--ToggleTrue" : "buttonTask__listItem--toggleFalse"}`}>
-               
-            </button>
-         <span className={`tasksList__content${task.done ? " tasksList__content--done" : ""}`}>
-            {task.content}
-         </span>
-         <button className="buttonTask buttonTask--remove">
-         </button>
+            <button
+               className={`buttonTask ${task.done
+                  ? "button__tasksListItem--ToggleTrue"
+                  : "buttonTask__listItem--toggleFalse"}`}
+            />
+            <span
+               className={`tasksList__content${task.done
+                  ? " tasksList__content--done"
+                  : ""}`}
+            >
+               {task.content}
+            </span>
+            <button className="buttonTask buttonTask--remove" />
          </li>
       ))}
    </ul>
