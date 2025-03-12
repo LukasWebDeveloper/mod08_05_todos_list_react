dfaba01d85bfbec160d46e5d7df41a6d4b8c0c6d
Lukasz Duszak <lukas94.webdeveloper@gmail.com>
2025-03-12 22:03:59 +0100

    commit styles changed

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,16 +1,24 @@
 import "./style.css";
 
-const Section = ({title, body, extraHeaderContent}) => (
-   <section className="section sectionAddTask">
-               <div className="div__titleTasksList">
-                  <h3 className="section__titleNewTask">{title}</h3>
-                  {extraHeaderContent}
-               </div>
-               <div className="div__tasksList">
-               {body}
-               </div>
-   </section>
-   
+const Section = ({ title, title1, body, body1, extraHeaderContent }) => (
+   <>
+      <section className="section sectionAddTask">
+         <div>
+            <h3 className="section__titleNewTask">{title1}</h3>
+         </div>
+         <div> {/*  className="div__tasksList" */}
+            {body1}
+         </div>
+      </section>
+      <section className="section sectionTasks">
+         <div className="div__tasksList">
+            <h3 className="section__titleTasksList">{title}</h3>
+            {extraHeaderContent}
+         </div>
+         {body}
+      </section>
+   </>
+
 );
 
 export default Section;
